d4977c37005646c8d8253bed79373b7974599334
mknguyen105 <30991432+mknguyen105@users.noreply.github.com>
2022-08-01 19:03:22 -0700

    Update MazeSolutionException.hpp

--- include/darkmaze/MazeSolutionException.hpp
+++ include/darkmaze/MazeSolutionException.hpp
@@ -1,8 +1,5 @@
 // MazeSolutionException.hpp
 //
-// ICS 46 Spring 2017
-// Project #1: Dark at the End of the Tunnel
-//
 // Exception thrown by a MazeSolution object when interacting with it in a
 // way that's invalid, such as backing up when the solution is already empty
 // or extending the solution outside of the boundaries of the maze.

f23bc0a4405d6970b01ec65b515466dc2eafb55a
mknguyen105 <30991432+mknguyen105@users.noreply.github.com>
2022-08-01 19:01:54 -0700

    Update Direction.hpp

--- include/darkmaze/Direction.hpp
+++ include/darkmaze/Direction.hpp
@@ -1,8 +1,5 @@
 // Direction.hpp
 //
-// ICS 46 Spring 2017
-// Project #1: Dark at the End of the Tunnel
-//
 // This enumeration allows you to specify directions of movement within a maze
 // (up, down, left, or right).  For example, the way you would specify the "up"
 // direction is like this:
